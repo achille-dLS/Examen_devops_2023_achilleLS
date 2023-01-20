315580e19a7fef772037146fec3a94b5c8097f8a
achille-dLS <74300561+achille-dLS@users.noreply.github.com>
2023-01-20 10:49:15 +0100

    ajout verif gamertag

--- src/services/__tests__/validations.test.js
+++ src/services/__tests__/validations.test.js
@@ -1,4 +1,5 @@
-const { isEmpty } = require("../validations");
+const { isEmpty, isValidGamertag} = require("../validations");
+
 
 describe("validations tests suites - isEmpty", () => {
     test("should return true as the label is undefined", () => {
@@ -17,4 +18,21 @@ describe("validations tests suites - isEmpty", () => {
     });
 });
 
-// TODO: Create tests suite for validation function
+// Tests pour vérifier si la fonction fonctionne correctement
+describe('isValidGamerTag', () => {
+    it('should return true for a valid gamer tag', () => {
+        expect(isValidGamertag('Player#1')).toBe(true);
+    });
+
+    it('should return false for a gamer tag with less than 8 characters', () => {
+        expect(isValidGamertag('Pl#1')).toBe(false);
+    });
+
+    it('should return false for a gamer tag without special characters', () => {
+        expect(isValidGamertag('PlayerOne')).toBe(false);
+    });
+
+    it('should return false for a gamer tag without numbers', () => {
+        expect(isValidGamertag('Player#One')).toBe(false);
+    });
+});
